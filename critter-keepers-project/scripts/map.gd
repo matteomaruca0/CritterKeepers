6680b185011f1d93a388d2049706af18cd24e16e
Matteo Maruca <matteo.maruca@student.supsi.ch>
2026-04-09 12:04:28 +0200

    Continuing main map (Lakes habitat)

--- critter-keepers-project/scripts/map.gd
+++ critter-keepers-project/scripts/map.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var prompt1 = $PierRock1/TravelPanel
 @onready var prompt2 = $PierRock2/TravelPanel
+@onready var prompt3 = $PierRock3/TravelPanel
 @onready var player = $Player
 
 func _ready():
@@ -12,6 +13,10 @@ func _ready():
 	#Molo 2
 	prompt2.scelta_nuota.connect(_on_nuota)
 	prompt2.scelta_vola.connect(_on_vola)
+	
+	#Molo 3
+	prompt3.scelta_nuota.connect(_on_nuota)
+	prompt3.scelta_vola.connect(_on_vola)
 
 #func _on_molo_trigger_body_entered(body):
 #	if body.is_in_group("player"):
