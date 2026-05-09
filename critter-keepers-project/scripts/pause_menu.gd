d19ac882e43ad9f6fbe280853694739e18ea3c43
Matteo Maruca <matteo.maruca@student.supsi.ch>
2026-05-09 14:27:23 +0200

    Fixing

--- critter-keepers-project/scripts/pause_menu.gd
+++ critter-keepers-project/scripts/pause_menu.gd
@@ -11,18 +11,21 @@ extends CanvasLayer
 @onready var btn_italiano = $PanelSettings/VBoxContainer/BtnItalian
 @onready var btn_inglese = $PanelSettings/VBoxContainer/BtnEnglish
 @onready var btn_chiudi_impostazioni = $PanelSettings/VBoxContainer/BtnClose
+@onready var checkbox_musica = $PanelSettings/VBoxContainer/CheckBoxMusic
 
 func _ready():
 	panel_menu.visible = false
 	panel_impostazioni.visible = false
 	btn_pausa.pressed.connect(_on_pausa)
-	btn_salva_esci.pressed.connect(_on_salva_esci)
+	#btn_salva_esci.pressed.connect(_on_salva_esci)
 	btn_esci.pressed.connect(_on_esci)
 	btn_impostazioni.pressed.connect(_on_impostazioni)
 	btn_italiano.pressed.connect(_on_italiano)
 	btn_inglese.pressed.connect(_on_inglese)
 	btn_chiudi_impostazioni.pressed.connect(_on_chiudi_impostazioni)
 	btn_close.pressed.connect(_on_chiudi)
+	checkbox_musica.button_pressed = true  # di default musica attiva
+	checkbox_musica.toggled.connect(_on_musica_toggled)
 	
 func _on_chiudi(): 
 	panel_menu.visible = false
@@ -53,3 +56,13 @@ func _on_inglese():
 func _on_chiudi_impostazioni():
 	panel_impostazioni.visible = false
 	get_tree().paused = false
+
+func _on_musica_toggled(attiva: bool):
+	print("toggled: ", attiva)
+	var musica = get_tree().get_first_node_in_group("background_music")
+	print("musica trovata: ", musica)
+	if musica:
+		if attiva:
+			musica.play()
+		else:
+			musica.stop()
